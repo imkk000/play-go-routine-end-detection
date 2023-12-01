3f2332bd229ea34d376c84dcbc262b9c538980a2
Nattakit Boonyang <nattakit.b@7solutions.co.th>
2023-12-01 11:13:29 +0700

    Update log message for stop with reason

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"time"
 )
@@ -27,35 +26,28 @@ func main() {
 
 	// watchdog
 	for stopMsg := range stopChan {
-		fmt.Printf("stop[%d]: err=%v\n", stopMsg.i, stopMsg.err)
-
 		go worker(stopChan, stopMsg.i)
+		fmt.Printf("worker id=%d stop with ", stopMsg.i)
+		if stopMsg.err != nil {
+			fmt.Printf("error is %v\n", stopMsg.err)
+			continue
+		}
+		fmt.Println("no reason")
 	}
 }
-
-var c int
 
 func worker(stopChan chan<- stopMsg, i int) {
 	defer func() {
 		var err error
 		if r := recover(); r != nil {
 			err = r.(error)
-			stopWithReason(stopChan, i, err)
-			return
 		}
-		stop(stopChan, i)
+		stopWithReason(stopChan, i, err)
 	}()
 
-	if c++; c%10 == 0 {
-		panic(errors.New("is mod 10"))
-	}
 	time.Sleep(time.Duration(i) * time.Second)
 }
 
 func stopWithReason(stopChan chan<- stopMsg, i int, reason error) {
 	stopChan <- stopMsg{i: i, err: reason}
-}
-
-func stop(stopChan chan<- stopMsg, i int) {
-	stopChan <- stopMsg{i: i}
 }
